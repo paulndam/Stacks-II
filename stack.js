f97b03816aa1ebd27a085da9a0f2fed283935e1e
Paul Ndam <paulndam@Pauls-MacBook-Air-2.local>
2021-03-10 13:48:09 -0500

    one

---
+++ stack.js
@@ -0,0 +1,50 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return this.size++;
+  }
+
+  pop() {
+    if (!this.first) return null;
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
+
+const stack = new Stack();
+stack.push("car");
+stack.push("cat");
+stack.push("dog");
+console.log(stack);
+console.log(`-------- 1st Pop ---------`);
+stack.pop();
+console.log(stack);
+console.log(`-------- 2st Pop ---------`);
+stack.pop();
+console.log(stack);
